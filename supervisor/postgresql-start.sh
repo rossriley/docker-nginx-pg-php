008256bc250299ddb9584807c721f4a0b8f39be6
Ross Riley <riley.ross@gmail.com>
2014-05-29 11:05:54 +0100

    improvements to the build and setup of psql

--- supervisor/postgresql-start.sh
+++ supervisor/postgresql-start.sh
@@ -1,5 +1,15 @@
 #/bin/bash
-chown -R postgres /var/lib/postgresql/9.3/main
-chmod -R 700 /var/lib/postgresql/9.3/main
+DATA_DIR=/data/pgsql
+
+# test if DATA_DIR has content
+if [[ ! "$(ls -A $DATA_DIR)" ]]; then
+    echo "Initializing PostgreSQL at $DATA_DIR"
+
+      # Copy the data that we generated within the container to the empty DATA_DIR.
+      cp -R /var/lib/postgresql/9.3/main/* $DATA_DIR
+fi
+
+chown -R postgres $DATA_DIR
+chmod -R 700 $DATA_DIR
 
 /etc/init.d/postgresql start
