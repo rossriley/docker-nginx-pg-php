fc710d243166839b0581371bdace7456d81fc216
Ross Riley <riley.ross@gmail.com>
2014-05-29 22:17:42 +0100

    add initdb to postgres start

--- supervisor/postgresql-start.sh
+++ supervisor/postgresql-start.sh
@@ -4,9 +4,8 @@ DATA_DIR=/data/pgsql
 # test if DATA_DIR has content
 if [[ ! "$(ls -A $DATA_DIR)" ]]; then
     echo "Initializing PostgreSQL at $DATA_DIR"
+    sudo -u postgres /usr/lib/postgresql/9.3/bin/initdb  -D /data/pgsql/
 
-      # Copy the data that we generated within the container to the empty DATA_DIR.
-      cp -R /var/lib/postgresql/9.3/main/* $DATA_DIR
 fi
 
 chown -R postgres $DATA_DIR
