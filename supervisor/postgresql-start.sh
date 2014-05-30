e41b7b741a3cf3637a0d525d58ff29bd38f8b77b
Ross Riley <riley.ross@gmail.com>
2014-05-30 08:51:34 +0100

    copy postgres data rather than recreating

--- supervisor/postgresql-start.sh
+++ supervisor/postgresql-start.sh
@@ -4,7 +4,7 @@ DATA_DIR=/data/pgsql
 # test if DATA_DIR has content
 if [[ ! "$(ls -A $DATA_DIR)" ]]; then
     echo "Initializing PostgreSQL at $DATA_DIR"
-    sudo -u postgres /usr/lib/postgresql/9.3/bin/initdb  -D /data/pgsql/
+    cp -R /var/lib/postgresql/9.3/main/* $DATA_DIR
 
 fi
 
