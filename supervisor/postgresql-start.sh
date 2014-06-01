8a75ff454952690c6558e12886cfcfcc1497ad80
Ross Riley <riley.ross@gmail.com>
2014-06-01 10:34:51 +0100

    added support for db setup and init

--- supervisor/postgresql-start.sh
+++ supervisor/postgresql-start.sh
@@ -5,7 +5,23 @@ DATA_DIR=/data/pgsql
 if [[ ! "$(ls -A $DATA_DIR)" ]]; then
     echo "Initializing PostgreSQL at $DATA_DIR"
     cp -R /var/lib/postgresql/9.3/main/* $DATA_DIR
+    
+    echo "Creating the superuser: $USER"
 
+    setuser postgres psql -q <<-EOF
+        DROP ROLE IF EXISTS $PG_USER;
+        CREATE ROLE $PG_USER WITH ENCRYPTED PASSWORD '$PG_PASS';
+        ALTER USER $PG_USER WITH ENCRYPTED PASSWORD '$PG_PASS';
+        ALTER ROLE $PG_USER WITH SUPERUSER;
+        ALTER ROLE $PG_USER WITH LOGIN;
+EOF
+    
+    
+    if [ $(env | grep PG_DB) ]; then
+        setuser postgres psql -q <<-EOF
+            CREATE DATABASE $PG_DB WITH OWNER=$PG_USER ENCODING='UTF8';
+            GRANT ALL ON DATABASE $db TO $PG_USER
+EOF
 fi
 
 chown -R postgres:postgres $DATA_DIR
